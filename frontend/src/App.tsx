acc12a54723861bd6cc1e4622f22a6dcf1fc7ca0
Adeyemi-cmd <falujoadeyemi6776@gmail.com>
2026-06-29 20:00:43 +0100

    feat(frontend): add evidence JSON export for reviewer follow-up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { useFreighter } from './hooks/useFreighter'
 import { useNetworkMode } from './lib/useNetworkMode'
 import { pingBackendWake } from './lib/wakeBackend'
 import { isMainnetEnabled } from './config/networks'
+import EvidenceExportAction from './components/EvidenceExportAction'
 import NetworkMismatchBanner from './components/NetworkMismatchBanner'
 import MainnetVersionBanner from './components/MainnetVersionBanner'
 import {
@@ -486,6 +487,8 @@ function App() {
               </div>
             </div>
           </div>
+
+          <EvidenceExportAction />
         </section>
 
         <section className="w-full lg:w-[640px] lg:max-w-none lg:justify-self-start">
